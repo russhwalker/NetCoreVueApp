94f80dc6994832c3392ff214c79a712c34517628
russell walker <russ.h.walker@gmail.com>
2018-12-10 08:09:52 -0500

    finishing implementing makeedit

--- src/NetCoreVueApp.Web/ClientApp/components/makeedit/makeedit.ts
+++ src/NetCoreVueApp.Web/ClientApp/components/makeedit/makeedit.ts
@@ -17,7 +17,25 @@ export default class MakeEditComponent extends Vue {
     }
 
     handleSave(event: any) {
-
+        this.editing = false;
+        var body = JSON.stringify(this.make);
+        fetch('api/Makes/Save', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: body
+        }).then(response => {
+            response.json();
+        })
+            .then(() => {
+                this.$router.push('/makes');
+            })
+            .catch(() => {
+                this.editing = true;
+                alert('ERROR');
+            });
     }
 
     mounted() {
